0e6dd4f9e28a4f6d28b5369628b40cfc1d6303d9
yano <x20a1224@gmail.com>
2018-06-26 14:45:38 +0900

    自分なりの完成

--- site1/blogs/edit.php
+++ site1/blogs/edit.php
@@ -5,17 +5,20 @@ $base -> titleDisplay();
 $base -> headerDisplay();
 ?>
 
-<p>記事タイトル</p>
-<form action="view.php" method="post">
-<input type="text" name="title">
-<p>記事内容</p>
-<textarea name="contents" cols="50" rows="10"></textarea>
-<p><input type="button" value="登録" onClick="location.href='http://localhost/lamp-blog/site1/blogs/view.php'"></p>
-</form>
+<body>
+	<p>記事タイトル</p>
+	<form action="view.php" method="post">
+	<input type="text" name="title">
+	<p>記事内容</p>
+	<textarea name="contents" cols="50" rows="10"></textarea>
+	<p><input type="button" value="登録" onClick="location.href='http://localhost/lamp-blog/site1/blogs/view.php'"></p>
+	</form>
 
-<p>登録日時　登録</p>
-<p>更新日時　更新</p>
+	<p>登録日時　登録</p>
+	<p>更新日時　更新</p>
 
-<p><input type="button" value="更新" onClick="location.href='http://localhost/lamp-blog/site1/blogs/view.php'"></p>
+	<p><input type="button" value="更新" onClick="window.location.reload();"></p>
 
-<p><a href="index.php">戻る</a></p>
+	<p><a href="index.php">戻る</a></p>
+</body>
+</html>
--- site1/blogs/view.php
+++ site1/blogs/view.php
@@ -4,6 +4,19 @@ $base = new Base();
 $base -> titleDisplay();
 $base -> headerDisplay();
 ?>
+<body>
+	<p><a href="edit.php">編集</a></p>
+	<p><a href="del.php">削除</a></p>
 
-<p><a href="index.php">編集</a></p>
-<p><a href="index.php">削除</a></p>
+	<p>--------------------------------------</p>
+	<p><a href="view.php">記事タイトル</a></p>
+	<p>--------------------------------------</p>
+
+	<p>記事内容</p>
+
+	<p>登録日時　登録</p>
+	<p>更新日時　更新</p>
+
+	<p><a href="index.php">戻る</a></p>
+</body>
+</html>
