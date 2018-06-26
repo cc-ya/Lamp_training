7ab9030176c4fc9f810ad020998681cd9e824933
yano <x20a1224@gmail.com>
2018-06-26 14:57:35 +0900

    完成

--- site1/blogs/del.php
+++ site1/blogs/del.php
@@ -11,7 +11,6 @@ $base -> headerDisplay();
 	<p>--------------------------------------</p>
 	<p>記事内容</p>
 	<p>記事登録日時</p>
-	<p><a href="view.php">記事タイトル</a></p>
 	<p><input type="button" value="削除" onClick="location.href='http://localhost/lamp-blog/site1/blogs/view.php'"></p>
 
 	<p><a href="index.php">戻る</a></p>
